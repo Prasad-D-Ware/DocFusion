2c355dea866faa0753445af8bd00f83e0f5861db
Prasad-D-Ware <prasadware42116@gmail.com>
2024-08-14 00:15:26 +0530

    feat: added clerk authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export const metadata: Metadata = {
   title: "DocFusion",
@@ -19,6 +21,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
+    <ClerkProvider appearance={
+      {
+        baseTheme:dark,
+        variables: {colorPrimary : "#3371FF", 
+          fontSize:
+          "16px"
+        } , 
+      }
+    }>
     <html lang="en" suppressHydrationWarning>
       <body
         className={cn("min-h-screen font-sans antialiased", fontSans.variable)}
@@ -26,5 +37,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
