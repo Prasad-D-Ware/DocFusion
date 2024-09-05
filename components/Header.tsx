a27e40898da9ad5a678e34c51b306e1adf7bfcc3
Prasad-D-Ware <prasadware42116@gmail.com>
2024-09-05 22:19:18 +0530

    fix : header styling fixed

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,25 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-const Header = ({children} : HeaderProps) => {
+const Header = ({ children }: HeaderProps) => {
   return (
-    <div className='header'>
-        <Link href={"/"} className='md:flex-1'>
-            <Image src={"/assets/icons/logo.svg"} alt='Logo with name' width={120} height={32} className='hidden md:block'/>
-            <Image src={"/assets/icons/logo-icon.svg"} alt='Logo without name' width={120} height={32} className='mr-2 md:hidden'/>
-        </Link>
-        {children}
-      
+    <div className="header">
+      <Link href={"/"} className="md:flex-1">
+        <div className="flex gap-1 items-center ">
+          <Image
+            src={"/assets/icons/logo-icon.svg"}
+            alt="Logo without name"
+            width={55}
+            height={14}
+            className=""
+          />
+          <h1 className="font-bold text-xl hidden md:block">DocFusion</h1>
+        </div>
+      </Link>
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
